da342194495717758c749fcb683b4d6241655558
Lucyfer Angel <50595135+LucyferHW@users.noreply.github.com>
2026-01-02 15:55:35 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,7 +5,7 @@ HTML_DIR="/etc/arma3/html"
 
 SCRIPTS="/etc/arma3/scripts"
 MOD_LIST="/etc/arma3/scripts/sub_list.txt"
-MOD_DIRECTORY="/home/arma3server/.local/share/Steam/steamapps/workshop/content/107410"
+MOD_DIRECTORY="/home/arma3server/Steam/steamapps/workshop/content/107410"
 
 # import steam user and password
 export STEAMUSER="$(cat /run/secrets/steam_user)"
